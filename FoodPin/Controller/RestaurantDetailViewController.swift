edc7db3617a805478ff3ed46e0c83e42c646da0c
sclo <sclo@gms.ndhu.edu.tw>
2020-12-03 11:58:23 +0800

    add map

--- FoodPin/Controller/RestaurantDetailViewController.swift
+++ FoodPin/Controller/RestaurantDetailViewController.swift
@@ -21,7 +21,7 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
         1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        3
+        5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -45,9 +45,23 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
         let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailTextCell.self), for: indexPath) as! RestaurantDetailTextCell
         cell.descriptionLabel.text = restaurant.description
         cell.selectionStyle = .none
-                    
+        
         return cell
-                    
+         
+        case 3:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailSeparatorCell.self), for: indexPath) as! RestaurantDetailSeparatorCell
+            cell.titleLabel.text = "HOW TO GET HERE"
+            cell.selectionStyle = .none
+            
+            return cell
+            
+        case 4:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailMapCell.self), for: indexPath) as! RestaurantDetailMapCell
+            cell.selectionStyle = .none
+            cell.configure(location: restaurant.location)
+            
+            return cell
+            
         default:
         fatalError("Failed to instantiate the table view cell for detail view controller")
         }
@@ -68,6 +82,13 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
 
         tableView.delegate = self
         tableView.dataSource = self
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showMap" {
+            let destinationController = segue.destination as! MapViewController
+            destinationController.restaurant = restaurant
+        }
     }
     
 
